90b8c320f6550b9cb8581e21e8cc025249f8ef39
Cris <111942487+Christjacoben@users.noreply.github.com>
2025-01-12 22:03:29 +0800

    Update appointmentSlice.js

--- src/store/appointmentSlice.js
+++ src/store/appointmentSlice.js
@@ -4,12 +4,6 @@ export const fetchAppointment = createAsyncThunk(
   "appointments/fetchAppointments",
   async () => {
      let token = sessionStorage.getItem("token");
-
-    if (!token) {
-      const cookies = document.cookie.split("; ");
-      const tokenCookie = cookies.find((row) => row.startsWith("token="));
-      token = tokenCookie ? tokenCookie.split("=")[1] : null;
-    }
     const response = await fetch(
       "https://vet-backend-m3o7.onrender.com/api/appointments",
       {
